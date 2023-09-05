2fbee0f8ff10322038896af57f04a1bfc98a29ba
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-05 10:22:54 +0300

    Cmpleted lab

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,23 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    session.query(Game).delete()
+    session.commit()
+
+    # Generate and add sample data to the "games" table
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.unique.name(),  # Use unique names to avoid duplicates
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)  # Random price between 0 and 60
+        )
+        for _ in range(50)  # Create 50 random game records
+    ]
+
+    session.bulk_save_objects(games)
+    session.commit()
+
+    print("Seeding completed.")
